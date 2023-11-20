3330d4ad9d49b5f1cea60ea2550456a9fbb3b8ef
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2023-11-20 20:49:25 -0300

    refact: change OutputTybe validation to remove memory allocation

--- src/OutputEnvelop/Exceptions/InvalidOutputType/InvalidOutputTypeException.cs
+++ src/OutputEnvelop/Exceptions/InvalidOutputType/InvalidOutputTypeException.cs
@@ -18,7 +18,20 @@ namespace MCIO.OutputEnvelop.Exceptions.InvalidOutputType
         // Builders
         public static void ThrowIfInvalid(OutputType outputType)
         {
-            if (!Enum.IsDefined(typeof(OutputType), outputType))
+            /*
+             * The bool IsDefined(Type enumType, Object value) alloc in heap
+             * https://github.com/microsoft/referencesource/blob/master/mscorlib/system/enum.cs#L596
+             * 
+             * To avoid these allocation, I decide couple the enum value range in this method
+             * instead of heap allocation.
+             * 
+             * Alternative code that alloc:
+             * 
+             * if (!Enum.IsDefined(typeof(OutputType), outputType))
+             *      throw new InvalidOutputTypeException(outputType);
+             */
+            var value = (int)outputType;
+            if (value < 0 || value > 3)
                 throw new InvalidOutputTypeException(outputType);
         }
     }
